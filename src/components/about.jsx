eba34c4d2744e616deeb82f421dd683abc19764a
demirgazihan <gazihandemir0@gmail.com>
2025-08-12 22:01:57 +0300

    removed about.png

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,11 +5,8 @@ export const About = (props) => {
     <div id="about">
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-md-6 text-center">
-            {" "}
-            <img src="img/about.jpg" alt="" />{" "}
-          </div>
-          <div className="col-xs-12 col-md-6">
+
+          <div className="col-xs-12">
             <div className="about-text">
               <h2>Hakkımızda</h2>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
